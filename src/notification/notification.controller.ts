c2188238d4439ba57e4d08ac02ffb6214d009072
Michał Dziuba <mail@michaldziuba.dev>
2023-07-07 11:57:20 +0200

    fix: controller

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -5,24 +5,8 @@ import { NotificationService } from './notification.service';
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
-  @Post('emails/send')
+  @Post('emails')
   sendEmail(@Body() body: { email: string; description: string }) {
     return this.notificationService.sendEmail(body.email, body.description);
-  }
-
-  @Post('topics/create')
-  createTopic(@Body() body: { key: string; name: string }) {
-    return this.notificationService.createTopic(body.key, body.name);
-  }
-
-  @Post('topics/send')
-  sendToTopic(
-    @Body() body: { topicKey: string; email: string; description: string },
-  ) {
-    return this.notificationService.sendToTopic(
-      body.topicKey,
-      body.email,
-      body.description,
-    );
   }
 }
